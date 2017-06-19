d65113c6654b89c502cd3ab28c05b2bcfa92193a
Hiroto Shioi <hiroto.shioi@iohk.io>
2017-06-19 11:17:33 +0900

    refacture code

--- bot.js
+++ bot.js
@@ -4,6 +4,70 @@ const formatPrice = value => {
   return require('numeral')(value).format('0,0.00[00000]');
 };
 
+const sendErrorMessage = (bot, message) => {
+    const errorReplyObject = {
+        "attachments": [
+            {
+                "fallback": "Something went wrong with the",
+                "pretext": "Check failed",
+                "title": "Cryptocurrency checker",
+                "text": "There was an error with CCC, please try again",
+                "color": "#cc0000"
+            }
+        ]
+    }
+    bot.replyPrivate(message, errorReplyObject);
+};
+
+const searchCurrency = (currency, bot , message) => {
+  const apiURL = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${currency}&tsyms=USD`;
+  require('request')(apiURL,(error, response, body) => {
+    if(error){
+      sendErrorMessage(bot, message);
+    } else {
+      currencyInformation = JSON.parse(body);
+      const { RAW } = currencyInformation;
+      if(!RAW){
+        sendErrorMessage(bot, message);
+      } else {
+        require('request')("https://www.cryptocompare.com/api/data/coinlist/",(error, response, body) => {      
+          const data = JSON.parse(body);
+          const { ImageUrl, CoinName } = data.Data[currency];
+          const { HIGH24HOUR, LOW24HOUR, PRICE, CHANGE24HOUR } = RAW[currency].USD;
+          const change = Math.floor(CHANGE24HOUR / PRICE * 10000) / 100;
+          const changeColor = (change < 0) ? "#CC0000" : "#2ab27b";
+          const successReplyObject = {
+              "attachments": [
+                  {
+                    "author_name":CoinName,
+                    "author_icon":`https://www.cryptocompare.com/${ImageUrl}`,
+                    "fallback": `Current rate for the ${currency} is $${formatPrice(PRICE)} - https://www.cryptocompare.com/`,
+                    "title": `$${formatPrice(PRICE)} (${change}%)`,
+                    "title_link": `https://www.cryptocompare.com/coins/${currency.toLowerCase()}/overview/USD`,
+                    "thumb_url":`https://www.cryptocompare.com/${ImageUrl}`,
+                    "color": changeColor,
+                    "fields": [
+                        {
+                            "title": "High",
+                            "value": `$${formatPrice(HIGH24HOUR)}`,
+                            "short": true
+                        },
+                        {
+                            "title": "Low",
+                            "value": `$${formatPrice(LOW24HOUR)}`,
+                            "short": true
+                        },
+                    ],
+                  }
+              ]
+          }
+          bot.replyPrivate(message, successReplyObject);
+        });
+      }
+    }
+  });
+}
+
 var controller = botkit.slackbot({
   debug: false,
   json_file_store: './simple_storage/'
@@ -28,64 +92,11 @@ controller.on('slash_command', function(bot, message) {
   switch (message.command) {
   case '/ccc':
     const currency = message.text.trim().toUpperCase();
-    const apiURL = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${currency}&tsyms=USD`;
-    require('request')(apiURL,(error, response, body) => {
-      if(error){
-        const errorMessage = `Error with the api, please try again`;
-        bot.replyPrivate(message, '<@' + message.user + '> *' +errorMessage+ '*');
-      } else {
-        currencyInformation = JSON.parse(body);
-        const { RAW } = currencyInformation;
-        if(!RAW){
-          const errorReplyObject = {
-              "attachments": [
-                  {
-                      "fallback": "Invalid input, please type in correct currency",
-                      "pretext": "Invalid currency",
-                      "title": "Crypto search",
-                      "text": "Currency was not found, please try again",
-                      "color": "#cc0000"
-                  }
-              ]
-          }
-          bot.replyPrivate(message, errorReplyObject);
-        } else {
-          require('request')("https://www.cryptocompare.com/api/data/coinlist/",(error, response, body) => {      
-            const data = JSON.parse(body);
-            const { ImageUrl, CoinName } = data.Data[currency];
-            const { HIGH24HOUR, LOW24HOUR, PRICE, CHANGE24HOUR } = RAW[currency].USD;
-            const change = Math.floor(CHANGE24HOUR / PRICE * 10000) / 100;
-            const changeColor = (change < 0) ? "#CC0000" : "#2ab27b";
-            const successReplyObject = {
-                "attachments": [
-                    {
-                      "author_name":CoinName,
-                      "author_icon":`https://www.cryptocompare.com/${ImageUrl}`,
-                      "fallback": `Current rate for the ${currency} is $${formatPrice(PRICE)} - https://www.cryptocompare.com/`,
-                      "title": `$${formatPrice(PRICE)} (${change}%)`,
-                      "title_link": `https://www.cryptocompare.com/coins/${currency.toLowerCase()}/overview/USD`,
-                      "thumb_url":`https://www.cryptocompare.com/${ImageUrl}`,
-                      "color": changeColor,
-                      "fields": [
-                          {
-                              "title": "High",
-                              "value": `$${formatPrice(HIGH24HOUR)}`,
-                              "short": true
-                          },
-                          {
-                              "title": "Low",
-                              "value": `$${formatPrice(LOW24HOUR)}`,
-                              "short": true
-                          },
-                      ],
-                    }
-                ]
-            }
-            bot.replyPrivate(message, successReplyObject);
-          });
-        }
-      }
-    });
+    if(message.command === "list"){
+
+    } else {
+      searchCurrency(currency, bot ,message);
+    }
     break;
   }
 });
