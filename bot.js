6223cdcc6e86454a3c094785a1ef222417d572e2
Hiroto Shioi <hiroto.shioi@iohk.io>
2017-06-20 15:52:41 +0900

    handle data fetching more nicely

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const formatPrice = (value, exchange) => {
 const fetchData = (url) => {
   const result = new Promise((resolve, reject) =>{
     require('request')(url, (error, response, body) => {
-      if (body){
+      if(!error){
         resolve(JSON.parse(body));
       } else {
         reject(JSON.parse(error));
